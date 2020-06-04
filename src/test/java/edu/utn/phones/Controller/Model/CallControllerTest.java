507c70f94b48d5e54ca64c3b1c1c10d9cd0ce43f
Kiiiu15 <kiiiu15@users.noreply.github.com>
2020-06-04 19:14:49 -0300

    .

--- src/test/java/edu/utn/phones/Controller/Model/CallControllerTest.java
+++ src/test/java/edu/utn/phones/Controller/Model/CallControllerTest.java
@@ -3,7 +3,6 @@ package edu.utn.phones.Controller.Model;
 import edu.utn.phones.Model.Call;
 import edu.utn.phones.Model.City;
 import edu.utn.phones.Service.CallService;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
